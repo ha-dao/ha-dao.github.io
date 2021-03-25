ab6fd0ab52357bdb27864a07dcaa729a23c7dd36
dcvh <70559610+dcvh@users.noreply.github.com>
2021-03-25 14:34:38 +0100

    1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,6 +24,45 @@
     }
   });
 
+
+
+
+
+	// Add class to mailto link
+	// Needed to separate the disabling of the default action AND copy email to clipboard
+	$('a[href^=mailto]').addClass('mailto-link');
+
+  var mailto = $('.mailto-link');
+	var messageCopy = 'Click to copy email address';
+	var messageSuccess = 'Email address copied to clipboard';
+
+// Disable opening your email client
+$('a[href^=mailto]').click(function() {
+  return false; 
+})
+
+// On click, get href and remove 'mailto:' from value
+// Store email address in a variable.
+mailto.click(function() {
+  var href = $(this).attr('href');
+  var email = href.replace('mailto:', '');
+  copyToClipboard(email);
+  $('.mailto-message').empty().append(messageSuccess);
+  setTimeout(function() {
+  $('.mailto-message').empty().append(messageCopy);}, 2000); 
+});
+
+// Copies the email variable to clipboard
+function copyToClipboard(text) {
+    var contactdata = document.createElement("input");
+    document.body.appendChild(contactdata);
+    contactdata.setAttribute('value', text);
+    contactdata.select();
+    document.execCommand('copy');
+    document.body.removeChild(contactdata);
+}
+
+
   $(".owl-carousel").owlCarousel({
     items: 4,
     lazyLoad: true,
